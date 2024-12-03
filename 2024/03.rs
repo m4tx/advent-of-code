d45ab3ed35a3cebd0d50f4b1ccbf08c859c9c143
Mateusz Maćkowski <m4tx@m4tx.pl>
2024-12-03 06:20:46 +0100

    feat: add day 3 solution

---
+++ 2024/03.rs
@@ -0,0 +1,65 @@
+use advent::prelude::*;
+
+type Input = Vec<(bool, i64, i64)>;
+
+fn parse_input(input: &str) -> Input {
+    let mut results = Vec::new();
+    let re = regex::Regex::new(r"^mul\((\d+),(\d+)\)").unwrap();
+
+    let mut enabled = true;
+    for i in 0..input.len() {
+        if input[i..].starts_with("do()") {
+            enabled = true;
+        } else if input[i..].starts_with("don't()") {
+            enabled = false;
+        } else if input[i..].starts_with("mul(") {
+            if let Some(captures) = re.captures(&input[i..]) {
+                let (_, [a, b]) = captures.extract();
+                results.push((
+                    enabled,
+                    i64::from_str_radix(a, 10).unwrap(),
+                    i64::from_str_radix(b, 10).unwrap(),
+                ));
+            }
+        }
+    }
+
+    results
+}
+
+fn default_input() -> Input {
+    parse_input(include_input!(2024 / 03))
+}
+
+fn part1(input: Input) -> i64 {
+    input.iter().map(|(_enabled, a, b)| a * b).sum()
+}
+
+fn part2(input: Input) -> i64 {
+    input
+        .iter()
+        .map(|(enabled, a, b)| if *enabled { a * b } else { 0 })
+        .sum()
+}
+
+fn main() {
+    let solution = advent::new(default_input).part(part1).part(part2).build();
+    solution.cli()
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(input.clone()), 1);
+    assert_eq!(part2(input), 2);
+}
+
+#[test]
+fn examples() {
+    let input =
+        parse_input("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+    assert_eq!(part1(input.clone()), 161);
+    let input =
+        parse_input("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+    assert_eq!(part2(input), 48);
+}
